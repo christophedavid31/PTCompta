98ebac4fa501a9597dace447f1b721a56038c33c
Christophe DAVID <chrdav81@free.fr>
2016-09-09 21:07:34 +0200

    added Sales list as NatTable
    Signed-off-by: Christophe DAVID <chrdav81@free.fr>

--- fr.logiprolls.ptcompta.ui/src/fr/logiprolls/ptcompta/ui/parts/SalesListPart.java
+++ fr.logiprolls.ptcompta.ui/src/fr/logiprolls/ptcompta/ui/parts/SalesListPart.java
@@ -1,19 +1,41 @@
 
 package fr.logiprolls.ptcompta.ui.parts;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.Persist;
 import org.eclipse.nebula.widgets.nattable.NatTable;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
+import org.eclipse.nebula.widgets.nattable.data.ListDataProvider;
+import org.eclipse.nebula.widgets.nattable.data.ReflectiveColumnPropertyAccessor;
+import org.eclipse.nebula.widgets.nattable.grid.data.DefaultColumnHeaderDataProvider;
+import org.eclipse.nebula.widgets.nattable.grid.data.DefaultCornerDataProvider;
+import org.eclipse.nebula.widgets.nattable.grid.data.DefaultRowHeaderDataProvider;
+import org.eclipse.nebula.widgets.nattable.grid.layer.ColumnHeaderLayer;
+import org.eclipse.nebula.widgets.nattable.grid.layer.CornerLayer;
+import org.eclipse.nebula.widgets.nattable.grid.layer.GridLayer;
+import org.eclipse.nebula.widgets.nattable.grid.layer.RowHeaderLayer;
+import org.eclipse.nebula.widgets.nattable.hideshow.ColumnHideShowLayer;
+import org.eclipse.nebula.widgets.nattable.layer.AbstractLayerTransform;
+import org.eclipse.nebula.widgets.nattable.layer.DataLayer;
+import org.eclipse.nebula.widgets.nattable.reorder.ColumnReorderLayer;
+import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer;
+import org.eclipse.nebula.widgets.nattable.viewport.ViewportLayer;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
+
+import fr.logiprolls.ptcompta.shop.Shop;
 
 public class SalesListPart {
+
+	private IDataProvider bodyDataProvider;
+	private String[] propertyNames;
+	private BodyLayerStack bodyLayer;
+	private Map<String, String> propertyToLabels;
 
 	@Inject
 	public SalesListPart() {
@@ -21,14 +43,26 @@ public class SalesListPart {
 	}
 
 	@PostConstruct
-	public void postConstruct(Composite parent) {
-		parent.setLayout(new GridLayout(1, false));
+	public void postConstruct(Composite parent, Shop shop) {
 
-		Label lblToto = new Label(parent, SWT.NONE);
-		lblToto.setText("List");
 
-		NatTable natTable = new NatTable(parent, SWT.BORDER | SWT.FULL_SELECTION);
-		natTable.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		bodyDataProvider = setupBodyDataProvider(shop);
+		DefaultColumnHeaderDataProvider colHeaderDataProvider = new DefaultColumnHeaderDataProvider(propertyNames,
+				propertyToLabels);
+		DefaultRowHeaderDataProvider rowHeaderDataProvider = new DefaultRowHeaderDataProvider(bodyDataProvider);
+
+		bodyLayer = new BodyLayerStack(bodyDataProvider);
+		ColumnHeaderLayerStack columnHeaderLayer = new ColumnHeaderLayerStack(colHeaderDataProvider);
+		RowHeaderLayerStack rowHeaderLayer = new RowHeaderLayerStack(rowHeaderDataProvider);
+		DefaultCornerDataProvider cornerDataProvider = new DefaultCornerDataProvider(colHeaderDataProvider,
+				rowHeaderDataProvider);
+		CornerLayer cornerLayer = new CornerLayer(new DataLayer(cornerDataProvider), rowHeaderLayer, columnHeaderLayer);
+
+        GridLayer gridLayer = new GridLayer(this.bodyLayer, columnHeaderLayer,
+                rowHeaderLayer, cornerLayer);
+        NatTable natTable = new NatTable(parent, gridLayer);
+
+
 	}
 
 	@Focus
@@ -41,5 +75,56 @@ public class SalesListPart {
 
 	}
 
+	@Inject
+	private IDataProvider setupBodyDataProvider(Shop shop) {
+
+		this.propertyToLabels = new HashMap<String, String>();
+		this.propertyToLabels.put("customer", "Customer");
+		this.propertyToLabels.put("description", "Description");
+		this.propertyToLabels.put("date", "Date");
+		this.propertyToLabels.put("value", "Value");
+		this.propertyToLabels.put("payments", "Payments");
+
+		this.propertyNames = new String[] { "customer", "description", "date", "payments" };
+		return new ListDataProvider(shop.getSales(), new ReflectiveColumnPropertyAccessor(this.propertyNames));
+
+	}
+
+	public class BodyLayerStack extends AbstractLayerTransform {
+
+		private SelectionLayer selectionLayer;
+
+		public BodyLayerStack(IDataProvider dataProvider) {
+			DataLayer bodyDataLayer = new DataLayer(dataProvider);
+			ColumnReorderLayer columnReorderLayer = new ColumnReorderLayer(bodyDataLayer);
+			ColumnHideShowLayer columnHideShowLayer = new ColumnHideShowLayer(columnReorderLayer);
+			this.selectionLayer = new SelectionLayer(columnHideShowLayer);
+			ViewportLayer viewportLayer = new ViewportLayer(this.selectionLayer);
+			setUnderlyingLayer(viewportLayer);
+		}
+
+		public SelectionLayer getSelectionLayer() {
+			return this.selectionLayer;
+		}
+	}
+
+	public class ColumnHeaderLayerStack extends AbstractLayerTransform {
+
+		public ColumnHeaderLayerStack(IDataProvider dataProvider) {
+			DataLayer dataLayer = new DataLayer(dataProvider);
+			ColumnHeaderLayer colHeaderLayer = new ColumnHeaderLayer(dataLayer, bodyLayer,
+					bodyLayer.getSelectionLayer());
+			setUnderlyingLayer(colHeaderLayer);
+		}
+	}
+
+	public class RowHeaderLayerStack extends AbstractLayerTransform {
+
+		public RowHeaderLayerStack(IDataProvider dataProvider) {
+			DataLayer dataLayer = new DataLayer(dataProvider, 50, 20);
+			RowHeaderLayer rowHeaderLayer = new RowHeaderLayer(dataLayer, bodyLayer, bodyLayer.getSelectionLayer());
+			setUnderlyingLayer(rowHeaderLayer);
+		}
+	}
 
 }
